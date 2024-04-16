f90d5ed6f47bd488190407ed65a4b7fb637a27d4
Smedten <miksmedt@gmail.com>
2024-04-16 10:46:11 +0200

    Implemented buffer for RLTS

---
+++ RLTS/buffer.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+
+class TrajectoryEnv:
+    def __init__(self, df, buffer_size=4):
+        self.buffer_size = buffer_size
+        self.k = 3
+        self.buffer = pd.DataFrame(columns=['latitude', 'longitude', 'value'])
+        self.current_index = 0
+        self.trajectory_error = 0
+        self.original_trajectory = df.assign(value=0.0)
+
+    def calculate_simplification_error(self):
+        return self.trajectory_error
+
+    def calculate_compression_ratio(self):
+        original_points = len(self.original_trajectory)
+        simplified_points = len(self.buffer)
+        return original_points / simplified_points
+
+    def ped(self, current, next_point, prev_point):
+        point, next_point, prev_point = current[['latitude', 'longitude']], next_point[['latitude', 'longitude']], prev_point[['latitude', 'longitude']]
+        if np.array_equal(next_point, prev_point):
+            return 0
+        return np.abs(np.cross(next_point-prev_point, prev_point-point)) / np.linalg.norm(next_point-prev_point)
+
+    def update_values(self, idx):
+        self.buffer.reset_index(drop=True, inplace=True)
+
+        print("index: ", idx)
+        if idx < 1 or idx > self.buffer_size-1:
+            print("Warning: wrong index given")
+
+        point = self.buffer.iloc[idx]
+        prev_point = self.buffer.iloc[max(idx - 1, 0)]
+        prev_prev_point = self.buffer.iloc[max(idx - 2, 0)]
+        next_point = self.buffer.iloc[min(idx + 1, len(self.buffer) - 1)]
+        next_next_point = self.buffer.iloc[min(idx + 2, len(self.buffer)-1)]
+
+        if (idx-1) != 0:
+            self.buffer.at[idx-1, 'value'] = max(self.ped(prev_point, next_point, prev_prev_point), self.ped(point, next_point, prev_prev_point))
+        else:
+            self.buffer.at[idx-1, 'value'] = 0
+
+        if (idx+1) != self.buffer_size + 1:
+            self.buffer.at[idx+1, 'value'] = max(self.ped(next_point, next_next_point, prev_point), self.ped(point, next_next_point, prev_point))
+        else:
+            self.buffer.at[idx+1, 'value'] = 0
+
+    def get_state(self):
+        if len(self.buffer) < self.k:
+            return np.array([]), []
+
+        errors = self.buffer['value'].iloc[1:-1].sort_values()[:self.k]
+        state = errors.values
+        indices = errors.index.tolist()
+        return state, indices
+
+    def step(self, action):
+        done = False
+        error_before = self.trajectory_error
+        state, indices = self.get_state()
+
+        if self.current_index < len(self.original_trajectory):
+            next_point = self.original_trajectory.iloc[self.current_index] # skal scannes som 0,0
+            current_point = self.original_trajectory.iloc[self.current_index-1] #skal have ny værdi
+            prev_point = self.original_trajectory.iloc[self.current_index-2]  #tidligere sidste værdi
+         #   print("POINT", current_point)
+
+            self.buffer.at[self.buffer_size, 'value'] = self.ped(current_point, next_point, prev_point)
+            self.buffer = self.buffer.reset_index(drop=True)
+            self.buffer = pd.concat([self.buffer, pd.DataFrame([next_point])])
+            self.buffer = self.buffer.reset_index(drop=True)
+
+        else:
+            done = True
+
+        index_to_drop = indices[action]
+        self.update_values(index_to_drop)
+        dropped_point = self.buffer['value'].iloc[index_to_drop]
+        self.buffer = self.buffer.drop(index_to_drop)
+        self.buffer = self.buffer.reset_index(drop=True)
+
+        self.trajectory_error = max(self.trajectory_error, dropped_point)
+
+        reward = -self.trajectory_error
+
+        self.current_index += 1
+
+        next_state, _ = self.get_state()
+        return next_state, reward, done
+
+    def reset(self):
+        self.trajectory_error = 0
+        self.buffer = self.original_trajectory.iloc[:self.buffer_size+1] # Include one extra for initial state
+        self.current_index = self.buffer_size+1
+        for i in range(1, self.buffer_size): #opdater værdier for p1, p2, p3
+            self.buffer.at[i, 'value'] = self.ped(self.buffer.iloc[i], self.buffer.iloc[i+1], self.buffer.iloc[i-1])
+        initial_state, _ = self.get_state()
+        return initial_state
